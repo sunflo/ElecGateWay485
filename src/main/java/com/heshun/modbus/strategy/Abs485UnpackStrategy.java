129dd1cba46a22534392e37d482aab2cce44e6d5
huangxz <huangxiaozheng@kedacom.com>
2019-06-20 11:29:09 +0800

    日志新增设备类型输出

--- src/main/java/com/heshun/modbus/strategy/Abs485UnpackStrategy.java
+++ src/main/java/com/heshun/modbus/strategy/Abs485UnpackStrategy.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.session.IoSession;
 
-import com.heshun.modbus.common.GloableStorage;
+import com.heshun.modbus.common.GlobalStorage;
 import com.heshun.modbus.entity.AbsJsonConvert;
 import com.heshun.modbus.entity.DefaultDevicePacket;
 import com.heshun.modbus.entity.PacketCrcIllagelException;
@@ -14,58 +14,60 @@ import com.heshun.modbus.util.ELog;
 import com.heshun.modbus.util.SessionUtils;
 
 public abstract class Abs485UnpackStrategy<T extends DefaultDevicePacket, V extends AbsJsonConvert<T>> {
-	protected IoSession mSession;
-	protected IoBuffer mBuffer;
+    IoSession mSession;
+    IoBuffer mBuffer;
 
-	public Abs485UnpackStrategy(IoSession s, IoBuffer b) {
-		this.mBuffer = b;
-		this.mSession = s;
-	}
+    public Abs485UnpackStrategy(IoSession s, IoBuffer b) {
+        this.mBuffer = b;
+        this.mSession = s;
+    }
 
-	public V unPack() throws PacketCrcIllagelException {
-		ELog.getInstance().log("原始报文:" + mBuffer.getHexDump(), mSession);
-		return getConvert(doUnpack());
-	}
+    public V unPack() throws PacketCrcIllagelException {
+        ELog.getInstance().log(String.format("原始报文[%s]:\n %s", getLogTag(), mBuffer.getHexDump()), mSession);
+        return getConvert(doUnpack());
+    }
 
-	@SuppressWarnings("unchecked")
-	protected T doUnpack() throws PacketCrcIllagelException {
-		// 预留CRC校验方法,后期可能要加上容错机制
-		if (!validateCrc()) {
-			throw new PacketCrcIllagelException();
-		}
+    protected String getLogTag() {
+        String simpleName = getClass().getSimpleName();
+        return simpleName.replace("Strategy", "");
+    }
 
-		int address = (int) mBuffer.get();
-		int command = (int) mBuffer.get();
+    protected T doUnpack() throws PacketCrcIllagelException {
+        // 预留CRC校验方法,后期可能要加上容错机制
+        if (!validateCrc())
+            throw new PacketCrcIllagelException();
 
-		int size = (int) (mBuffer.get() & 0xff);
+        int address = (int) mBuffer.get();
+        int command = (int) mBuffer.get();
 
-		String signature = String.format("%s-%s", command, size);
-		List<Byte> packet = new ArrayList<Byte>();
-		for (int i = 0; i < size; i++) {
-			packet.add(mBuffer.get());
-		}
-		// 消费掉两个字节的CRC校验
-		mBuffer.get();
-		mBuffer.get();
+        int size = mBuffer.get() & 0xff;
 
-		int logotype = SessionUtils.getLogoType(mSession);
+        String signature = String.format("%s-%s", command, size);
+        List<Byte> packet = new ArrayList<>();
+        for (int i = 0; i < size; i++)
+            packet.add(mBuffer.get());
+        // 消费掉两个字节的CRC校验
+        mBuffer.get();
+        mBuffer.get();
 
-		AbsJsonConvert<?> convert = GloableStorage.getInstance().getConvertInStorage(logotype, address,
-				initConvert(address));
-		convert.updateGatherTime();
+        int logotype = SessionUtils.getLogoType(mSession);
 
-		return handle(signature, packet, (V) convert);
+        AbsJsonConvert<? extends DefaultDevicePacket> convert = GlobalStorage.getInstance().getConvertInStorage(logotype, address,
+                initConvert(address));
+        convert.updateGatherTime();
 
-	}
+        return handle(signature, packet, (V) convert);
 
-	private boolean validateCrc() {
-		return true;
-	}
+    }
 
-	protected abstract T handle(String signature, List<Byte> packet, V convert);
+    private boolean validateCrc() {
+        return true;
+    }
 
-	public abstract V getConvert(T packet);
+    protected abstract T handle(String signature, List<Byte> packet, V convert);
 
-	public abstract V initConvert(int address);
+    public abstract V getConvert(T packet);
+
+    public abstract V initConvert(int address);
 
 }
