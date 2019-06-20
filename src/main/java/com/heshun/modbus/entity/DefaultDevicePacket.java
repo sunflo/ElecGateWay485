ffff3871725943684a6580d6043571eba6cc66ce
huangxz <huangxiaozheng@kedacom.com>
2019-06-20 09:29:57 +0800

    继承map，方便追加属性

--- src/main/java/com/heshun/modbus/entity/DefaultDevicePacket.java
+++ src/main/java/com/heshun/modbus/entity/DefaultDevicePacket.java
@@ -1,14 +1,16 @@
 package com.heshun.modbus.entity;
 
+import java.util.HashMap;
+
 /**
  * 预留，提供公用属性
- * 
+ *
  * @author huangxz
- * 
  */
-public class DefaultDevicePacket {
-	public int address = 0;
-	public DefaultDevicePacket(int address) {
-		this.address = address;
-	}
+public class DefaultDevicePacket extends HashMap<String, Object> {
+    public int address;
+
+    public DefaultDevicePacket(int address) {
+        this.address = address;
+    }
 }
