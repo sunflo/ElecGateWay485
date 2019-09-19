7bc757a3dac7a3706fbbcd45612ee4c436886227
huangxz <huangxiaozheng@kedacom.com>
2019-09-19 14:57:27 +0800

    修复json 转换丢失key的问题

--- src/main/java/com/heshun/modbus/strategy/common/harmonic/HarmonicDataWrapper.kt
+++ src/main/java/com/heshun/modbus/strategy/common/harmonic/HarmonicDataWrapper.kt
@@ -1,83 +1,73 @@
 package com.heshun.modbus.strategy.common.harmonic
 
-import com.heshun.modbus.entity.driver.DeviceDriver
 import com.heshun.modbus.strategy.common.CommonDevicePack
 
-class HarmonicDataWrapper(val mDevicePack: CommonDevicePack) {
+class HarmonicDataWrapper {
 
     val harmonicMap: Map<String, Array<Any>>
     //实时总有功需量
-    var pkn: Any? = null
+    var pkn: Any = 0
     //预测总有功需量
-    var pkf: Any? = null
+    var pkf: Any = 0
     //当前最大有功需量
-    var pkm: Any? = null
+    var pkm: Any = 0
     //上次最大有功需量
-    var pkl: Any? = null
+    var pkl: Any = 0
 
-    //谐波畸变率
-    var tdd_a: Any? = null
-    var tdd_b: Any? = null
-    var tdd_c: Any? = null
-    var tdd_odd_a: Any? = null
-    var tdd_odd_b: Any? = null
-    var tdd_odd_c: Any? = null
-    var tdd_even_a: Any? = null
-    var tdd_even_b: Any? = null
-    var tdd_even_c: Any? = null
+    var bphd_u: Any = 0
+    var bphd_i: Any = 0
+
 
     init {
         harmonicMap = HashMap()
         harmonicMap.apply {
-            put("h_i_a", Array(30) { Any() })
-            put("h_i_b", Array(30) { Any() })
-            put("h_i_c", Array(30) { Any() })
-            put("h_u_a", Array(30) { Any() })
-            put("h_u_b", Array(30) { Any() })
-            put("h_u_c", Array(30) { Any() })
+            put("h_i_a", Array(33) { 0 })
+            put("h_i_b", Array(33) { 0 })
+            put("h_i_c", Array(33) { 0 })
+            put("h_u_a", Array(33) { 0 })
+            put("h_u_b", Array(33) { 0 })
+            put("h_u_c", Array(33) { 0 })
         }
     }
 
-    fun ratio(key: String, mDriver: DeviceDriver): Any? {
-        return mDevicePack.remove(key)?.let { withRatio(it, mDriver[key]!!.ratio) }
-    }
 
-    fun filter(): HarmonicDataWrapper {
-        val mDriver = mDevicePack.mDriver
-
-        pkn = ratio("pkn", mDriver)
-
-        pkn = ratio("pkn", mDriver)
-        pkf = ratio("pkf", mDriver)
-        pkm = ratio("pkm", mDriver)
-        pkl = ratio("pkl", mDriver)
-
-        tdd_a = ratio("tdd_a", mDriver)
-        tdd_b = ratio("tdd_b", mDriver)
-        tdd_c = ratio("tdd_c", mDriver)
-        tdd_odd_a = ratio("dd_odd_a", mDriver)
-        tdd_odd_b = ratio("dd_odd_b", mDriver)
-        tdd_odd_c = ratio("dd_odd_c", mDriver)
-        tdd_even_a = ratio("tdd_even_a", mDriver)
-        tdd_even_b = ratio("tdd_even_b", mDriver)
-        tdd_even_c = ratio("tdd_even_c", mDriver)
-
-
+    fun filter(pack: CommonDevicePack): HarmonicDataWrapper {
+        val mDriver = pack.mDriver
         //
-        val it = mDevicePack.iterator()
+        val it = pack.iterator()
         while (it.hasNext()) {
             val next = it.next()
             val key = next.key ?: continue
 
-            if (key.startsWith("h_")) {
+            val driverItem = mDriver[key] ?: continue
+            val tag = driverItem.tag
+            if (tag in arrayOf("pkn", "pkf", "pkm", "pkl", "bphd_u", "bphd_i")) {
+                when (tag) {
+                    "pkn" -> pkn = withRatio(next.value, driverItem.ratio)
+                    "pkf" -> pkf = withRatio(next.value, driverItem.ratio)
+                    "pkm" -> pkm = withRatio(next.value, driverItem.ratio)
+                    "pkl" -> pkl = withRatio(next.value, driverItem.ratio)
+                    "bphd_u" -> bphd_u = withRatio(next.value, driverItem.ratio)
+                    "bphd_i" -> bphd_i = withRatio(next.value, driverItem.ratio)
+                }
+                it.remove()
+            }
+
+            if (tag.startsWith("h_")) {
                 //h_u_29_a
-                val s = key.split("_")
-                harmonicMap["${s[0]}_${s[1]}_${s[3]}"]?.set(s[2].toInt(), withRatio(next.value, mDriver[key]?.ratio
-                        ?: 0))
+                val s = tag.split("_")
+                harmonicMap["${s[0]}_${s[1]}_${s[3]}"]?.set(s[2].toInt(), withRatio(next.value, driverItem.ratio))
+                it.remove()
+            }
+
+            // 6,4,1,1,thd_u_c_o,3-212,8
+            if (tag.startsWith("thd_")) {
+                val s = tag.split("_")
+                val index = if (s.size <= 3) 32 else if (s[3] == "o") 0 else 1
+                harmonicMap["h_${s[1]}_${s[2]}"]?.set(index, withRatio(next.value, driverItem.ratio))
                 it.remove()
             }
         }
-
         return this
     }
 
--- src/main/java/com/heshun/modbus/strategy/common/harmonic/HarmonicExtraInfoConvertDelegate.kt
+++ src/main/java/com/heshun/modbus/strategy/common/harmonic/HarmonicExtraInfoConvertDelegate.kt
@@ -1,5 +1,6 @@
 package com.heshun.modbus.strategy.common.harmonic
 
+import com.alibaba.fastjson.JSON
 import com.alibaba.fastjson.JSONObject
 import com.heshun.modbus.strategy.common.CommonDevicePack
 import com.heshun.modbus.strategy.common.IntfExtraInfoDelegate
@@ -10,7 +11,9 @@ import com.heshun.modbus.strategy.common.IntfExtraInfoDelegate
 class HarmonicExtraInfoConvertDelegate : IntfExtraInfoDelegate<CommonDevicePack> {
     override fun handle(origin: JSONObject, pack: CommonDevicePack): JSONObject {
 
-        origin["harmonic"] = HarmonicDataWrapper(pack).filter()
+        val o = HarmonicDataWrapper().filter(pack)
+        val json = JSON.toJSON(o)
+        origin["harmonic"] =json as JSONObject
 
         val mDriver = pack.mDriver
         for (entry in pack.entries) {
@@ -30,7 +33,7 @@ class HarmonicExtraInfoConvertDelegate : IntfExtraInfoDelegate<CommonDevicePack>
                 is Short -> if (ratio > 0) o.toFloat() * ratio else o.toFloat() / ratio
                 is Double -> if (ratio > 0) o * ratio else o / ratio
                 is Float -> if (ratio > 0) o * ratio else o / ratio
-                is Long -> if (ratio > 0) o.toDouble() * ratio.toLong() else o.toDouble() / ratio.toLong()
+                is Long -> (if (ratio > 0) o.toDouble() * ratio.toLong() else o.toDouble() / ratio.toLong()).toLong()
                 else -> o
             }
 
